14c417c7fb471dcb7b7138f831b451f9d585f164
Yue Weng Mak <yueweng.mak@gmail.com>
2020-01-31 11:54:26 -0800

    update code

--- wine.py
+++ wine.py
@@ -12,14 +12,9 @@ from datetime import datetime
 
 class Wine():
   def __init__(self, url):
-        PROXY = "23.23.23.23:3128" # IP:PORT or HOST:PORT
-
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--proxy-server=%s' % PROXY)
-
         self.url = url
         self.path = r'/Users/yuewengmak/Desktop/chromedriver'
-        self.driver = webdriver.Chrome(executable_path = self.path, options=chrome_options)
+        self.driver = webdriver.Chrome(executable_path = self.path)
 
   def setup_driver(self):
 
