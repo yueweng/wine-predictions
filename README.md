a62731ed22df0b3368d599c591dc39fcc8e07ebf
Yue Weng Mak <yueweng.mak@gmail.com>
2020-02-05 11:29:35 -0800

    update README file

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 ![](images/wine.jpg)
 
-## Context
+## Introduction
+
+In this project, I will predict the prices of the wines based on numerous factors, like vintage, country, ratings, type of grapes.
+
+## Data Collection
+I am using Selenium to scrape the data from Vivino.com, a popular wine review site, where there are over more than 100,000 wines. I used MongoDB as my database to store the data and convert it to DataFrame for analysis. I am focusing on a few popular countries that produce wines, with ratings above 3.5 stars and a price range of up to $250.
+
+## Cleaning the data
+After scraping the data, some prices are returning null since the price is not present on the site. I converted these null values to 0 and took out these rows so that my model will not take into account wines with $0 prices. 
+
+## Modeling
+
+I looked into a few models to predict the wine prices.
+
+### Linear Regression
+The most basic model I go with since the result is continuous.
+
+![](images/linear_regression.png)
+
+### Logistic Regression
+
+I set a few thresholds for this model, setting a 0 if the price is below a certain threshold and 1 if the price is above a certain threshold
+
+![](images/roc_curve.png)
+
+
--- wine.py
+++ wine.py
@@ -8,6 +8,7 @@ class Wine():
   def __init__(self):
       self.collection = None
       self.df = None
+      self.dummies = None
 
   def get_wine_data(self):
     client = MongoClient('localhost', 27017)
@@ -40,7 +41,21 @@ class Wine():
 
   def create_vintage_column(self):
     self.df['vintage'] = self.df['title'].apply(lambda x: int(x.split(' ')[-1]) if self.representsInt(x.split(' ')[-1]) else None)
-    return self
+    return self.df
+
+  def create_grape_column(self):
+    self.df['grapes'] = None
+
+    grapes = ['Chardonnay', 'Riesling', 'Malbec', 'Pinot Noir', 'Merlot', 'Sauvignon Blanc', 'Cabernet Sauvignon', 'Shiraz', 'Syrah']
+
+    for idx, row in self.df.iterrows():
+        for g in grapes:
+            if g in row['title']:
+                self.df.loc[idx, 'grapes'] = g
+    return self.df
+
+  def fill_null_value(self, col):
+    self.df[col].fillna(0, inplace=True)
 
   def representsInt(self, s):
     try: 
@@ -49,31 +64,71 @@ class Wine():
     except ValueError:
         return False
 
-  def plot_type_based_on_popularity(self):
-    ratings_wine = self.df.groupby('type').agg({'num_ratings': 'count'}).reset_index()
-    ratings_wine.sort_values(by=['num_ratings'], ascending=False, inplace=True)
+  def plot_graph(self, x, y, title, xlabel, ylabel, image_url, fontsize=20, pad=20):
     fig, ax = plt.subplots(figsize=[20, 10])
-    ax.bar(ratings_wine['type'], ratings_wine['num_ratings'])
-    ax.set_title('Wine Types based on Popularity', fontsize=20, pad=20)
-    ax.set_xlabel('Wine Types', fontsize=20)
-    ax.set_ylabel('Num of Ratings', fontsize=20)
-    plt.savefig('images/wine_types_popularity.png')
+    ax.bar(x, y)
+    ax.set_title(title, fontsize=fontsize, pad=pad)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    ax.set_ylabel(ylabel, fontsize=fontsize)
+    plt.savefig(image_url)
+
+  def plot_type_based_on_popularity(self):
+    ratings_wine = self.df.groupby('type').agg({'num_ratings': 'count', 'price': 'mean'}).reset_index()
+    ratings_wine.sort_values(by=['num_ratings'], ascending=False, inplace=True)
+    self.plot_graph(ratings_wine['type'], ratings_wine['num_ratings'], 'Wine Types based on Popularity', 'Wine Types', 'Num of Ratings', 'images/wine_types_popularity.png')
+
+    ratings_wine.sort_values(by='price', ascending=False, inplace=True)
+    self.plot_graph(ratings_wine['type'], ratings_wine['price'], 'Wine Types based on Price', 'Wine Types', 'Price', 'images/wine_types_price.png')
+
+    # fig, ax = plt.subplots(figsize=[20, 10])
+    # ax.scatter(ratings_wine['num_ratings'], ratings_wine['price'])
+
 
   def plot_country_based_on_type_popularity(self, ty):
     wine_df = self.df
     type_df = wine_df[wine_df['type'] == ty]
-    type_df = type_df.groupby('country').agg({'num_ratings': 'count'}).reset_index()
+    type_df = type_df.groupby('country').agg({'num_ratings': 'count', 'price': 'mean'}).reset_index()
     type_df.sort_values(by='num_ratings', ascending=False, inplace=True)
-    
-    fig, ax = plt.subplots(figsize=[20, 10])
-    ax.bar(type_df['country'], type_df['num_ratings'])
-    ax.set_xlabel('Country', fontsize=20)
-    ax.set_ylabel('Num of Ratings', fontsize=20) 
-    ax.set_title('{} Wine Based on Popularity'.format(ty), fontsize=20, pad=20)
-    plt.savefig('images/{}_country_types_popularity.png'.format(ty.lower()))
+
+    self.plot_graph(type_df['country'], type_df['num_ratings'], '{} Wine Based on Popularity'.format(ty), 'Country', 'Num of Ratings', 'images/{}_country_types_popularity.png'.format(ty.lower()))
+
+    type_df.sort_values(by='price', ascending=False, inplace=True)
+    self.plot_graph(type_df['country'], type_df['price'], '{} Wine Based on Price'.format(ty), 'Country', 'Price', 'images/{}_country_types_price.png'.format(ty.lower()))
 
   def country_rating_plot(self):
     types_wine = ['Sparkling', 'Red', 'White', 'Rosé', 'Dessert', 'Port']
 
     for ty in types_wine:
       self.plot_country_based_on_type_popularity(ty)
+
+  def get_us_wines(self):
+    us_df = self.df[self.df['country'] == 'United States']
+    return us_df
+
+  def plot_us_type_wine(self, us_df):
+    us_type = us_df.groupby('type').agg({'num_ratings': 'sum', 'ratings': 'mean'}).reset_index()
+    us_type.sort_values(by=['num_ratings'], ascending=False, inplace=True)
+
+    self.plot_graph(us_type['type'], us_type['num_ratings'], 'Wine Types based in United States', 'Type', 'Num of Ratings', 'images/wine_types_us.png')
+
+  def plot_red_white_us_wine(self, us_df):
+    types = ['Red', 'White']
+
+    for t in types:
+      color_us_df = us_df[us_df['type'] == t]
+      color_type = color_us_df.groupby('grapes').agg({'num_ratings': 'sum'}).reset_index()
+      color_type.sort_values(by=['num_ratings'], ascending=False, inplace=True)
+
+      self.plot_graph(color_type['grapes'], color_type['num_ratings'], '{} Wine based on Grapes in the United States'.format(t), 'Grapes', 'Num Ratings', 'images/{}_wine_grapes_us.png')
+  
+  def get_dummies(self, col):
+    wine_df = self.df
+    self.dummies = pd.get_dummies(wine_df[col])
+    return self
+
+  def combine_dummies(self, *args):
+    shorten_df = self.df.drop(columns=['location', 'region', 'image_url', 'reviews'])
+    for dummy in args:
+      shorten_df = pd.concat([shorten_df, dummy], axis=1)
+
+    return shorten_df
